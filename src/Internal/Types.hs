8eaa4627361d6b42eb60f7f6ff0abebdc626d12e
mwelt <michael.welt@ubmw.de>
2019-12-15 04:30:36 +0100

    Changed token to unsigned int and provide binary.Put and binary.Get convenience

--- src/Internal/Types.hs
+++ src/Internal/Types.hs
@@ -2,9 +2,37 @@ module Internal.Types
   (
     Token,
     Sentence,
-    Document
+    Document,
+    putToken,
+    putSentence,
+    getToken,
+    getSentence
   ) where
 
-type Token = Int
+import qualified External.Types as Ext 
+import Control.Monad
+
+import Data.Binary.Put
+import Data.Binary.Get
+import Data.Word
+
+type Token = Word32
 type Sentence = [Token]
 type Document = [Sentence]
+
+putToken :: Token -> Put 
+putToken = putWord32host
+
+putSentence :: Sentence -> Put 
+putSentence s = do
+  let len = length s 
+  putWord16host . fromIntegral $ len 
+  mapM_ putToken s
+
+getToken :: Get Token
+getToken = getWord32host
+
+getSentence :: Get Sentence
+getSentence = do
+  len <- getWord16host 
+  replicateM (fromIntegral len) getToken  
