a27d7606efb4ae0293e86977ba821f44249cea8a
mwelt <michael.welt@ubmw.de>
2019-12-18 16:06:20 +0100

    Qualified Dictionary as D

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-import Dictionary
+import qualified Dictionary as D
 import Types
 import qualified InvertedIndex as II
 import qualified SentencePersistence as SP
@@ -55,7 +55,7 @@ invertedIndexTest = do
     go doc = do
       d' <- mapM addSentence doc
       SP.close
-      int2ext <- getDictionaryInt2Ext 
+      int2ext <- D.getDictionaryInt2Ext 
       let keys = IM.keys int2ext
       mapM_ printInvertedIndex $ take 4 keys
       pure ()
@@ -63,18 +63,18 @@ invertedIndexTest = do
     printInvertedIndex :: Int -> AppT IO ()
     printInvertedIndex t = do
       let t' = fromIntegral t :: Int.Token
-      mt'' <- (translateToken t') :: AppT IO (Maybe Ext.Token)
+      mt'' <- (D.translateToken t') :: AppT IO (Maybe Ext.Token)
       liftIO $ do
         putStr . show $ mt''
         putStr ": "
       sIds <- IS.toList <$> II.get t'
       sentences <- mapM (SP.get . fromIntegral) sIds
-      sentences' <- mapM translateSentence sentences :: AppT IO Ext.Document 
+      sentences' <- mapM D.translateSentence sentences :: AppT IO Ext.Document 
       liftIO $ mapM_ (putStrLn . show) sentences'
     
     addSentence :: Ext.Sentence -> AppT IO Int.Sentence
     addSentence ss = do
-      ss' <- mapM addToken ss 
+      ss' <- mapM D.addToken ss 
       sId <- SP.append ss'
       II.put sId ss'
       pure ss'
@@ -91,13 +91,13 @@ sentenceEquality = do
     go :: Ext.Document -> AppT IO () 
     go doc = do
       -- translate
-      d' <- translateDocument doc 
+      d' <- D.addDocument doc 
       sentenceIds <- mapM SP.append d'
       SP.close
-      Dictionary.close
+      D.close
       --liftIO $ mapM_ (putStrLn . show) sentenceIds 
       mapM_ (SP.get
-             >=> (translateSentence :: Int.Sentence -> AppT IO Ext.Sentence)
+             >=> (D.translateSentence :: Int.Sentence -> AppT IO Ext.Sentence)
              >=> liftIO . putStrLn . show)
         $ take 10 sentenceIds
   
@@ -109,23 +109,23 @@ dictionaryEquality = do
     Left e -> liftIO $ putStrLn $ "Error: " ++ (show e)
     Right d -> do 
       -- translate and print document
-      d' <- (translateDocument :: Ext.Document -> AppT IO Int.Document) d
+      d' <- (D.translateDocument :: Ext.Document -> AppT IO Int.Document) d
       liftIO $ putStrLn . show $ d'
 
       -- close the file
-      Dictionary.close
+      D.close
 
       -- get the current dictionaries
-      int2ext <- getDictionaryInt2Ext
-      ext2int <- getDictionaryExt2Int
+      int2ext <- D.getDictionaryInt2Ext
+      ext2int <- D.getDictionaryExt2Int
 
       -- reread the dictionaries from disk
-      fromPersistence 
+      D.fromPersistence 
       -- int2ext' <- getDictionaryInt2Ext
       -- ext2int' <- getDictionaryExt2Int
 
       -- translate the document back
-      d'' <- translateDocument d'  
+      d'' <- D.translateDocument d'  
       liftIO $ putStrLn . show $ d == d''
 
 sampleData = "sample.txt" :: String
